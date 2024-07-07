410b4e74223dcace937789fcdf271ffa340f8e53
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2024-07-07 20:52:36 +0200

    Update (clean) Class1.cs
    cleaned up the code

--- UnConFix/Class1.cs
+++ UnConFix/Class1.cs
@@ -11,27 +11,12 @@ namespace UnConFix
     {
         public event CommandInputHandler inputCommitted;
 
-        /*
-event CommandInputHandler ICommandInputOutput.inputCommitted
-{
-add
-{
-// throw new NotImplementedException();
-}
-
-remove
-{
-// throw new NotImplementedException();
-}
-}*/
-        //CommandWindow cmdWin;
         bool act = true;
         List<String> cmdQ = new List<String>();
         void IModuleNexus.initialize()
         {
             Console.WriteLine("Custom JH Module initiating");
             Dedicator.commandWindow.addIOHandler(this);
-            //inputCommitted += Test;
             Task.Run(() => {
                 Console.WriteLine("Custom JH CMD listener activated.");
                 String cmd;
@@ -41,10 +26,6 @@ remove
                     {
                         if (!Commander.execute(default(CSteamID), cmd.Substring(4))) { CommandWindow.LogErrorFormat("Unable to match \"" + cmd.Substring(4) + "\" with any built-in commands"); };
                     }
-                    //Console.WriteLine("[COMMAND] >> " + cmd);
-                    //execute(CSteamID.Nil, cmd);
-                    //if (!Commander.execute(default(CSteamID), cmd)) { CommandWindow.LogErrorFormat("Unable to match \"" + cmd + "\" with any built-in commands"); };
-                    //inputCommitted.Invoke(cmd);
                     cmdQ.Add(cmd);
                 }
             });
@@ -52,35 +33,33 @@ remove
 
         void ICommandInputOutput.initialize(CommandWindow commandWindow)
         {
-            //cmdWin = commandWindow;
             Console.WriteLine("[2] Custom JH Module loading");
         }
 
         void ICommandInputOutput.outputError(string error)
         {
-            //Console.Error.WriteLine(error);
         }
 
         void ICommandInputOutput.outputInformation(string information)
         {
-            //Console.WriteLine(information);
         }
 
         void ICommandInputOutput.outputWarning(string warning)
         {
-            //Console.WriteLine("[WARN] "+warning);
         }
 
         void IModuleNexus.shutdown()
         {
-            Dedicator.commandWindow.removeIOHandler(this);
+            Console.WriteLine("Custom JH Module unloading");
+            if(act) Dedicator.commandWindow.removeIOHandler(this);
             Console.WriteLine("Custom JH Module unloaded");
             act = false;
         }
 
         void ICommandInputOutput.shutdown(CommandWindow commandWindow)
         {
-            Dedicator.commandWindow.removeIOHandler(this);
+            Console.WriteLine("[2] Custom JH Module unloading");
+            if(act) Dedicator.commandWindow.removeIOHandler(this);
             Console.WriteLine("[2] Custom JH Module unloaded");
             act = false;
         }
