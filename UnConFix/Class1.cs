5dba252f5127f1c02c69aa5382c576844ec43544
JGTV <22498524+JonnygamingTv@users.noreply.github.com>
2026-05-05 14:56:10 +0200

    Add files via upload

--- UnConFix/Class1.cs
+++ UnConFix/Class1.cs
@@ -11,7 +11,7 @@ namespace UnConFix
     public class DynamicTPS : IModuleNexus
     {
         int targetTPS = 10;
-        static int sleepTPS = 1;
+        static int sleepTPS = 2;
         public static int Sleep_TPS
         { 
             get => sleepTPS; 
